bf2be8f558e7287a4f6dde858cfe00e1f2ea8694
midudev <miduga@gmail.com>
2021-08-08 20:39:36 +0200

    Fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
@@ -43,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "warn",
     "react/prop-types": 0
   }
 }
