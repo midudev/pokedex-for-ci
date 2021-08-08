41f8589b6434b96c846ff67ceb212c70ff4ba46c
midudev <miduga@gmail.com>
2021-08-08 21:24:40 +0200

    Add healthcheck and do rollback if not working

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
