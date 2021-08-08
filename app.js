9c4cfe420e998fceefea9418b3d8e311de3a9cc7
midudev <miduga@gmail.com>
2021-08-08 21:01:18 +0200

    Add E2E tests to deployment pipeline

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
