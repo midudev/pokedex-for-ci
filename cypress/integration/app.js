0633b16836f9c74739e9c80527cf31e853597f78
DESKTOP-251FVN2\Cristalex <crisscresjm@gmail.com>
2022-05-04 19:23:13 +0200

    error corregido en cypress/integration/app.js

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -1,9 +1,10 @@
+/* eslint-disable linebreak-style */
 /* global cy */
 
 describe('Proyecto', function () {
-    it('frontpage can be opened', function () { // nos aseguramos que la página se pueda abrir
-        cy.visit('http://localhost:5000'); // página que tiene que visitar
-        cy.contains('pikachu'); // si la página contiene a pikachu
-        cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo') // si en la página aparece esta frase
-    }) 
+  it('frontpage can be opened', function () { // nos aseguramos que la página se pueda abrir
+    cy.visit('http://localhost:5000') // página que tiene que visitar
+    cy.contains('pikachu') // si la página contiene a pikachu
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo') // si en la página aparece esta frase
+  })
 })
