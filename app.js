dc2d8f29c4f3be1af48ae5fe89c7ce75d0a1e565
DESKTOP-251FVN2\Cristalex <crisscresjm@gmail.com>
2022-05-05 12:10:03 +0200

    healthcheck y rollback añadidos

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 /* eslint-disable linebreak-style */
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
