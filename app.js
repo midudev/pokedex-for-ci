b92c6bc61cb2e68c239f56023d1baeb5bbca97a7
MatiasTK <matiavallejo@gmail.com>
2022-06-01 21:52:17 -0300

    feat: added healthcheck and rollback

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const { version } = require('./package.json')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
+
+// Health check
+app.get('/health', (req,res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req,res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
