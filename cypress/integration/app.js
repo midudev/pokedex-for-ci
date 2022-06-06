d2a2a9ca10366e37ccbd3ea7eb6fb72cdc023ca1
Franco M <82059717+FrM-bot@users.noreply.github.com>
2022-06-06 00:58:56 -0300

    Delete cypress/integration directory

--- cypress/integration/app.js
+++
@@ -1,9 +0,0 @@
-/* global cy */
-
-describe('Pokedex', function() {
-  it('frontpage can be opened', function () {
-    cy.visit('http://localhost:5000')
-    cy.contains('pikachu')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
-  })
-})
