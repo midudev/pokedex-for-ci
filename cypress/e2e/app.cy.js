35ee5e1d708baa73d96487093c21f9663d5b83ca
Lucas Nicolas Chiesa Sturla <lchiesa@factorit.com.ar>
2022-08-29 16:41:07 -0300

    Adding cypress to project and pipeline

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,8 @@
+/* global cy */
+describe('Pokedex', function() {
+  it('Frontpage can be opened', function(){
+    cy.visit('http://localhost:8080')
+    cy.contains('pikachu')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
