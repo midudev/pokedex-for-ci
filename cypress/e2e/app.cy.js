911301a2e367d8425e288e5ce7fe444db0697753
Lucas Nicolas Chiesa Sturla <lchiesa@factorit.com.ar>
2022-08-29 17:17:35 -0300

    Testing 2nd run from e2e tests

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,6 +3,7 @@ describe('Pokedex', function() {
   it('Frontpage can be opened', function(){
     cy.visit('http://localhost:3001')
     cy.contains('pikachu')
+    cy.contains('Flareon')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
