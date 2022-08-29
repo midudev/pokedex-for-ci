d4c2371720e7adf5b18818b19fbdda06d7a13c8d
Lucas Nicolas Chiesa Sturla <lchiesa@factorit.com.ar>
2022-08-29 17:06:45 -0300

    Trying to solve cypress job

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,7 @@
 /* global cy */
 describe('Pokedex', function() {
   it('Frontpage can be opened', function(){
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3001')
     cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
