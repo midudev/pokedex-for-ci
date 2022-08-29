361142c02f4eb513df3c26c75146da6e4f06f6b7
Lucas Nicolas Chiesa Sturla <lchiesa@factorit.com.ar>
2022-08-29 19:19:05 -0300

    taking out breakline linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,10 +24,6 @@ module.exports = {
       "error",
         2
       ],
-    // "linebreak-style": [
-    //   "error",
-    //   "unix"
-    // ],
     "quotes": [
       "error",
       "single"
