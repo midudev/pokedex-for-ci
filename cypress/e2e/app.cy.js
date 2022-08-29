59f4151fe14d259327917bacbd9defad1f93672c
Lucas Nicolas Chiesa Sturla <lchiesa@factorit.com.ar>
2022-08-29 16:59:23 -0300

    changing port on cypress test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,7 @@
 /* global cy */
 describe('Pokedex', function() {
   it('Frontpage can be opened', function(){
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
