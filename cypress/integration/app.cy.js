53a6e0da9b8fe38200ab8d3c49c4f9cf4ec4c797
unknown <magp2606@gmail.com>
2022-09-30 01:25:26 -0500

    Fixed lint erros on app.cy.js file

--- cypress/integration/app.cy.js
+++ cypress/integration/app.cy.js
@@ -1,8 +1,11 @@
-/*global cy*/ 
-describe('Pokedex' , function() {
+/* global cy */
+
+describe('Pokedex', function () {
   it('frontpage can be opened', function () {
-    cy.visit('http://localhost:5000')
-    cy.contains('pikachu')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-})
+    cy.visit('http://localhost:5000');
+    cy.contains('pikachu');
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    );
+  });
+});
