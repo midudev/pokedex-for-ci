dc3f3a6ef87576e5831b8ed02545d18eea05ce7d
JereHendz <jereomarr@gmail.com>
2022-11-12 06:14:33 -0600

    eslint testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,9 @@ module.exports = {
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
-  // "plugins": [
-  //   react
-  // ],
+  "plugins": [
+    "react", "jest"
+  ],
   "rules": {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
