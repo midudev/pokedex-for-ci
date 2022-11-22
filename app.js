8cf22578e2736439947909d16975cc26e8cf3799
Diego C <usuario@MacBook-Pro-de-Usuario.local>
2022-11-22 16:18:54 -0500

    adding healthcheck and fixing deploy problems

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('CHIMBA')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
