4f4bce27e0f4959f80a6c22c6a1b98fc2c3e6e36
Hector Barazorda <ph.barazorda@gmail.com>
2023-12-31 22:03:55 -0500

    feat:

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 'warning',
+    'no-console': 'warn',
     'react/prop-types': 0
   }
 }
